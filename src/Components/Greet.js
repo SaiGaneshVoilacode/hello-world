61548f41595c23774a5020cb00876a00bf71402d
SaiGaneshVoilacode <d7@voilacode.com>
2024-01-11 05:17:35 +0530

    first commit

---
+++ src/Components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello Ganesh</h1>
+// }
+
+export const Greet = () => <h1>Hello Ganesh</h1>
+
+//export default Greet (Its a simple way for exporting rsther than above)
